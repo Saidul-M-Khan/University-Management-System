24f71b3fba77cb2bbd8b15d0452042fc9d89b7fb
Saidul-M-Khan <19-41766-3@student.aiub.edu>
2023-12-15 21:47:50 +0600

    query builder implemented

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -4,68 +4,77 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 import { Student } from './student.model';
+import QueryBuilder from '../../builder/QueryBuilder';
+import { studentSearchableFields } from './student.constant';
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query }; // copy of original query
+  // const queryObj = { ...query }; // copy of original query
   // remove the given fields from excludeFields array
-  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
-  excludeFields.forEach((element) => delete queryObj[element]);
-  console.log({query: query}, {queryObj: queryObj});
+  // const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+  // excludeFields.forEach((element) => delete queryObj[element]);
+  // console.log({query: query}, {queryObj: queryObj});
 
   // Searching
-  const studentSearchableFields = ['email', 'name.firstName', 'presentAddress'];
-  let searchTerm = '';
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string;
-  }
-  const searchQuery = Student.find({
-    $or: studentSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    })),
-  });
+  // const studentSearchableFields = ['email', 'name.firstName', 'presentAddress'];
+  // let searchTerm = '';
+  // if (query?.searchTerm) {
+  //   searchTerm = query?.searchTerm as string;
+  // }
+  // const searchQuery = Student.find({
+  //   $or: studentSearchableFields.map((field) => ({
+  //     [field]: { $regex: searchTerm, $options: 'i' },
+  //   })),
+  // });
 
   // Filtering
-  const filterQuery = searchQuery.find(queryObj);
+  // const filterQuery = searchQuery.find(queryObj);
 
   // Sorting
-  let sort = '-createdAt';
-  if (query?.sort) {
-    sort = query.sort as string;
-  }
-  const sortQuery = filterQuery.sort(sort);
+  // let sort = '-createdAt';
+  // if (query?.sort) {
+  //   sort = query.sort as string;
+  // }
+  // const sortQuery = filterQuery.sort(sort);
 
   // Limiting
-  let limit = 1;
-  if (query?.limit) {
-    limit = Number(query.limit);
-  }
-  const limitQuery = sortQuery.limit(limit);
+  // let limit = 1;
+  // if (query?.limit) {
+  //   limit = Number(query.limit);
+  // }
+  // const limitQuery = sortQuery.limit(limit);
 
   // Pagination
-  let page = 1;
-  let skip = 0;
-  if(query?.page){
-    page = Number(query.page);
-    skip = (page-1)*limit;
-  }
-  const paginateQuery = limitQuery.skip(skip);
+  // let page = 1;
+  // let skip = 0;
+  // if(query?.page){
+  //   page = Number(query.page);
+  //   skip = (page-1)*limit;
+  // }
+  // const paginateQuery = limitQuery.skip(skip);
 
   // Fields Limiting
-  let fields = '-__v'
-  if(query?.fields){
-    fields = (query.fields as string).split(',').join(' ');
-    console.log({fields});
-  }
-  const fieldLimitingQuery = paginateQuery.select(fields);
+  // let fields = '-__v'
+  // if(query?.fields){
+  //   fields = (query.fields as string).split(',').join(' ');
+  //   console.log({fields});
+  // }
+  // const fieldLimitingQuery = paginateQuery.select(fields);
 
   // Returning Response
-  const result = await fieldLimitingQuery.populate('admissionSemester').populate({
-    path: 'academicDepartment',
-    populate: {
-      path: 'academicFaculty',
-    },
-  });
+  // const result = await fieldLimitingQuery.populate('admissionSemester').populate({
+  //   path: 'academicDepartment',
+  //   populate: {
+  //     path: 'academicFaculty',
+  //   },
+  // });
+  // return result;
+
+  const studentQuery = new QueryBuilder(Student.find(), query).search(studentSearchableFields).filter().sort().paginate().fields();
+
+  const result = await studentQuery.modelQuery;
+
   return result;
+
 };
 
 const getSingleStudentFromDB = async (id: string) => {
