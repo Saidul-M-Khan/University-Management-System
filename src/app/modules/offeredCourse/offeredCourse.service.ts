6da3d29275f9fd3204a94e6edca7dbb29c9797c8
Saidul-M-Khan <19-41766-3@student.aiub.edu>
2023-12-21 03:28:08 +0600

    Semester registration and offerd course management added

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -229,10 +229,10 @@ const deleteOfferedCourseFromDB = async (id: string) => {
     throw new AppError(httpStatus.NOT_FOUND, 'Offered Course not found');
   }
 
-  const semesterRegistation = isOfferedCourseExists.semesterRegistration;
+  const semesterRegistration = isOfferedCourseExists.semesterRegistration;
 
   const semesterRegistrationStatus =
-    await SemesterRegistration.findById(semesterRegistation).select('status');
+    await SemesterRegistration.findById(semesterRegistration).select('status');
 
   if (semesterRegistrationStatus?.status !== 'UPCOMING') {
     throw new AppError(
