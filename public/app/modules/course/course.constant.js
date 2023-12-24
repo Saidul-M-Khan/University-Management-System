c5574f5dbdeaf41c6aec0e1342175b28906bb97e
Saidul Mursalin Khan <62834389+Saidul-M-Khan@users.noreply.github.com>
2023-12-24 22:53:12 +0600

    Delete public/app directory

--- public/app/modules/course/course.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CourseSearchableFields = void 0;
-exports.CourseSearchableFields = ['title', 'prefix', 'code'];
